7ab25efd45e6ed19951c2a2e3675329d0635471d
Hideaki Kawai <kayhide@gmail.com>
2020-05-27 11:11:23 +0900

    Resytle nix config

--- default.nix
+++ default.nix
@@ -1,27 +1,26 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+
 let
-  nixpkgs = import <nixpkgs> {};
-  inherit (nixpkgs) stdenv lib fetchurl autoconf boost systemd libusb1;
+  inherit (pkgs) stdenv lib fetchurl autoconf boost systemd libusb1;
   inherit (lib) getLib optional;
-
-  mayu = stdenv.mkDerivation {
-    pname = "mayu";
-    version = "0.12.1";
-
-    src = ./.;
-
-    buildInputs = [ boost boost.out systemd libusb1 ];
-
-    nativeBuildInputs = [ autoconf ];
-
-    preConfigure = ''
-      autoconf
-    '';
-
-    configureFlags = [
-      "--with-boost=${boost.out}"
-    ];
-
-  };
-
 in
-  mayu
+
+stdenv.mkDerivation {
+  pname = "mayu";
+  version = "0.12.1";
+
+  src = ./.;
+
+  buildInputs = [ boost boost.out systemd libusb1 ];
+
+  nativeBuildInputs = [ autoconf ];
+
+  preConfigure = ''
+    autoconf
+  '';
+
+  configureFlags = [
+    "--with-boost=${boost.out}"
+  ];
+}
